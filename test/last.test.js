c93e61c19f9a1b353cf4472bb1dabb58028dbee6
lichinshao <lichinshao@gmail.com>
2017-05-06 11:15:19 -0700

    added tests to last.test.js

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,16 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns last element of array if you ask for last 0 elements', () => {
+    expect(_.last([1, 2, 3, 4]), 0).toEqual(4);
+  });
+
+  it('returns last element of an array if n = -1', () => {
+    expect(_.last([1, 2, 3, 4]), -1).toEqual(4);
+  });
+
+  it('returns empty array if given n < -1', () => {
+    expect(_.last([1, 2, 3, 4, 5], -2)).toEqual([]);
+  });
 });

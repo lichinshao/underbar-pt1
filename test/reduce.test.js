bfadcb69345895d6f4c54943f27bc062632ebcba
lichinshao <lichinshao@gmail.com>
2017-05-07 09:59:35 -0700

    added tests to reduce.test.js

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -13,4 +13,22 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('reduces an array of numbers, with accumulator = 2 (initial sum value = 2 and not 0)', () => {
+    const nums = [0, 1, 2, 3, 4];
+    const result = _.reduce(nums, (a, b) => a + b, 2);
+    expect(result).toEqual(12);
+  });
+
+  it('reduces an array of numbers, with acculator = 0', () =>{
+    const nums = [0, 1, 2, 3, 4, 5];
+    const result = _.reduce(nums, (a, b) => a + b, 0);
+    expect(result).toEqual(15);
+  });
+
+  it('reduces an array of numbers, with acc = 10 (acc = initial sum value)', () => {
+    const nums = [0, 1, 2, 3, 4, 5];
+    const result = _.reduce(nums, (a, b) => a + b, 10);
+    expect(result).toBe(25);
+  });
+
 });

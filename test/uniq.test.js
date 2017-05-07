421db527bda6417e3334788944bf4704c5ef3e4a
lichinshao <lichinshao@gmail.com>
2017-05-07 11:53:28 -0700

    added test to uniq.test.js

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,14 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('gets rid of duplicates in array', () => {
+    const arr = [0, 0, 'dog', 'bert', 'dog', 0, 3, 4, 3, 3, 'horse'];
+    expect(_.uniq(arr)).toEqual([0, 'dog', 'bert', 3, 4, 'horse']);
+  });
+
+  it('gets all unique values in an object', () => {
+    const obj = {name: 'bob', age: 34, dogName: 'bob', dogYears: 34};
+    expect(_.uniq(obj)).toEqual(['bob', 34]);
+  });
 });

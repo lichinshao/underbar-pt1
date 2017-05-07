d67e462f8a4caa971570b68b5e73f1c392a23454
lichinshao <lichinshao@gmail.com>
2017-05-07 10:49:29 -0700

    added test to contains.test.js

--- test/contains.test.js
+++ test/contains.test.js
@@ -31,4 +31,20 @@ describe('contains()', () => {
     expect(_.contains(ponies, 'Applejack')).toBe(false);
   });
 
+  it('returns true if array contains target element', () => {
+    const nums = [0, 2, 4, 6, 8];
+    expect(_.contains(nums, 4)).toBe(true);
+  });
+
+  it('returns false if array does NOT contain element', () => {
+    const nums = [0, 2, 4, 6, 8];
+    expect(_.contains(nums, 5)).toEqual(false);
+  });
+
+  it('return true is value is in object and false if not in object', () => {
+    const object = {'name': 'Rob', 'age': 43, 'occupation': 'teacher'};
+    expect(_.contains(object, 'teacher')).toBe(true);
+    expect(_.contains(object, 'daughter')).toBe(false);
+  });
+
 });

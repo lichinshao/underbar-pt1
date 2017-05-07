b62800f21eea455adf9d901faf50663068516209
lichinshao <lichinshao@gmail.com>
2017-05-07 11:21:17 -0700

    added test to some.test.js

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,23 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if array contains any strings', () => {
+    const arr = [1, 2, 3, 'hi', 4, 5];
+    expect(_.some(arr, el => typeof el === 'string')).toBe(true);
+  });
+
+  it('returns false if NONE of the elements are strings', () => {
+    const arr = [1, 2, 3, 4];
+    expect(_.some(arr, el => typeof el === 'string')).toBe(false);
+  });
+
+  it('returns true if any object\'s values are divisble by 4', () => {
+    const object = {1: 17, 2: 31, 3: 28, 5: 101};
+    expect(_.some(object, value => value % 4 === 0)).toBe(true);
+  });
+
+  it('returns false if NONE of the objects values are divisible by 4', () => {
+    const object = {1: 3, 2: 5, 3: 27, 4: 71};
+    expect(_.some(object, value => value % 4 === 0)).toBe(false);
+  })
 });
